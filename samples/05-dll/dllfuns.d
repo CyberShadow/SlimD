895a13d57b3643f4efa8380213053ddbdc3b27c1
Vladimir Panteleev <vladimir@thecybershadow.net>
2014-10-22 21:42:55 +0000

    Add some comments to the DLL samples.

--- samples/05-dll/dllfuns.d
+++ samples/05-dll/dllfuns.d
@@ -1,4 +1,8 @@
 /// Actual functions declared by this DLL.
+/// We place the exported functions in a separate
+/// module from the DLL entry point, so that this
+/// module can be imported by DLL users (see
+/// 06-dlluser).
 
 module dllfuns;
 
--- samples/06-dlluser/dlluser.d
+++ samples/06-dlluser/dlluser.d
@@ -1,3 +1,13 @@
+/// SlimD example of linking to a D DLL.
+module dlluser;
+
+/// We import the module from the DLL implementation which
+/// defines the DLL functions. However, we only import it -
+/// the module will not be compiled or linked with the DLL.
+/// This allows us to use the same declaration for both the
+/// DLL implementation and interface. Otherwise, we'd need to
+/// have a separate .di file, or use extern(C) declarations
+/// to declare the functions defined in the DLL.
 import dllfuns;
 
 extern(C)
